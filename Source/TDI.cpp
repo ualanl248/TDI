41e02896b8a5f1fc7e95371d97e4aae45df98ca0
Adolfo Nogués López <adolfo.nogueslopezz@gmail.com>
2026-03-31 12:33:10 +0200

    Añado traza de porcentaje de umbral para pruebas

--- Source/TDI.cpp
+++ Source/TDI.cpp
@@ -41,7 +41,7 @@ int main()
 
 	imagen.Read("C:/Users/adolf/OneDrive/Desktop/PruebasTDI/CSgris.bmp");
 
-	//Normalización con algoritmo de min y max con recorte de percentiles (Umbral del 5%)
+	//Normalización con algoritmo de min y max con recorte de percentiles 
 	//Calcular el histograma e histograma acumulado de la imagen
 
 	int histograma[256] = { 0 };
@@ -56,39 +56,44 @@ int main()
 
 	int totalPixeles = imagen.LastRow() * imagen.LastCol();
 
-	double umbralBajo = totalPixeles * 0.95;
+	double umbralBajo = totalPixeles * 0.05;
 
-	double umbralAlto = totalPixeles * 0.05;
+	double umbralAlto = totalPixeles * 0.98;
 	
-	double pBajo;
+	double pBajo = 0;
 
 	for (int i = 0; i <= 255; i++) {
 		if (histogramaAcumulado[i] >= umbralBajo) {
-			double pBajo = i;
+			pBajo = i;
 			break;
 		}
 	}
 
-	double pAlto;
+	double pAlto = 0;
 
 	for (int i = 0; i <= 255; i++) {
 		if (histogramaAcumulado[i] >= umbralAlto) {
-			double pAlto = i;
+			pAlto = i;
 			break;
 		}
 	}
 	
 	//Aplicar algortimo min y max
 
-	C_Image imagenNormalizada;
+	C_Image imagenNormalizada(imagen.FirstRow(), imagen.LastRow(),imagen.FirstCol(), imagen.LastCol());
 
-	for (int i = 0; i <= imagenNormalizada.LastRow(); i++) {
-		for (int j = 0; j <= imagenNormalizada.LastCol(); j++) {
-			imagenNormalizada(i, j) = (imagen(i, j) - pBajo) * 255 / (pAlto - pBajo);
+	for (int i = imagen.FirstRow(); i <= imagenNormalizada.LastRow(); i++) {
+		for (int j = imagen.FirstCol(); j <= imagenNormalizada.LastCol(); j++) {
+			double valor = (double)(imagen(i, j) - pBajo) * 255.0 / (double)(pAlto - pBajo);
+			if (valor < 0) valor = 0;
+			if (valor > 255) valor = 255;
+			imagenNormalizada(i, j) = (int)valor;
 		}
 	}
 	
-	imagenNormalizada.Write("C:/Users/adolf/OneDrive/Desktop/PruebasTDI/CSgrisNormalizado.bmp");
-	//imagen.Write("C:/Users/adolf/OneDrive/Desktop/PruebasTDI/CSgris.bmp");
+	double porcentaje = 100 - ((umbralAlto) / totalPixeles * 100.0);
+	char ruta[256];
+	snprintf(ruta, sizeof(ruta), "C:/Users/adolf/OneDrive/Desktop/PruebasTDI/CSgrisNormalizado(%.0f%%).bmp", porcentaje);
+	imagenNormalizada.Write(ruta);
 	
 }
